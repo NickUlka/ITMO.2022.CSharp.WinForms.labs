1e1a54de17063706c81a326699b28c39a20f0adb
NickUlka <russkaya2011@gmail.com>
2022-07-08 11:09:52 +0300

    update

--- ITMO.2022.CSharp.WinForms.lab2.6/Form1.cs
+++ ITMO.2022.CSharp.WinForms.lab2.6/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyClass;
+
+
+
 
 namespace ITMO._2022.CSharp.WinForms.lab2._6
 {
@@ -15,6 +19,113 @@ namespace ITMO._2022.CSharp.WinForms.lab2._6
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        List<Item> its = new List<Item>();
+        public string Author // автор
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+        public string Title // Название
+        {
+            get { return textBox2.Text; }
+            set { textBox2.Text = value; }
+        }
+
+        public string PublishHouse // Издательство
+        {
+            get { return textBox3.Text; }
+            set { textBox3.Text = value; }
+        }
+
+        public int Page // Количество страниц
+        {
+            get { return (int)numericUpDown1.Value; }
+            set { numericUpDown1.Value = value; }
+        }
+        public int Year // Год издания
+        {
+            get { return (int)numericUpDown2.Value; }
+            set { numericUpDown2.Value = value; }
+        }
+        public int InvNumber // Инвентарный номер
+        {
+            get { return (int)numericUpDown3.Value; }
+            set { numericUpDown3.Value = value; }
+        }
+        public bool Existence // Наличие
+        {
+            get { return checkBox1.Checked; }
+            set { checkBox1.Checked = value; }
+        }
+        public bool SortInvNumber // Сортировка по инвентарному номеру
+        {
+            get { return checkBox2.Checked; }
+            set { checkBox2.Checked = value; }
+        }
+        public bool ReturnTime // Возвращение в срок
+        {
+            get { return checkBox3.Checked; }
+            set { checkBox3.Checked = value; }
+        }
+        public int PeriodUse // Инвентарный номер
+        {
+            get { return (int)numericUpDown4.Value; }
+            set { numericUpDown4.Value = value; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        //создайте объект класса Book, передав в конструктор свойства 
+        //формы:
+
+            Book b = new Book(Author, Title, PublishHouse,
+             Page, Year, InvNumber, Existence);
+
+            // реализуйте проверку возврата книги в срок:
+            if (ReturnTime)
+                b.ReturnSrok();
+
+            //И расчет стоимости с учетом срока пользования книгой
+            b.PriceBook(PeriodUse);
+
+
+            //добавьте книгу в список
+            its.Add(b);
+
+            //очистите поля ввода для новой информации
+            //(будьте внимательны, укажите значения, входящие в допустимый диапазон, 
+            //который вы настраивали ранее)
+            Author = Title = PublishHouse = "";
+            Page = 4;
+            InvNumber = 1;
+            PeriodUse = 1;
+            Year = 1900;
+            Existence = ReturnTime = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //проверьте состояние флажка сортировки и в
+            //случае его установки отсортируйте список:
+            if (SortInvNumber)
+                its.Sort();
+
+            //Для вывода информации создайте строку класса StringBuilder и с 
+            // помощью цикла постройте строку и информацией о единице хранения:
+            StringBuilder sb = new StringBuilder();
+            foreach (Item item in its)
+            {
+                sb.Append("\n" + item.ToString());
+            }
+
+            //После построения строки выведете ее в элемент richTextBox1:
+             richTextBox1.Text = sb.ToString();
         }
     }
 }
---
+++ ITMO.2022.CSharp.WinForms.lab5.1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ITMO._2022.CSharp.WinForms.lab5._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(311, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Chose the file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(522, 181);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // ofd
+            // 
+            this.ofd.DefaultExt = "\"*.xls;*.xlsx\"";
+            this.ofd.FileName = "openFileDialog1";
+            this.ofd.Title = "\"Выберите документ для загрузки данных\"";
+            this.ofd.FileOk += new System.ComponentModel.CancelEventHandler(this.ofd_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog ofd;
+    }
+}
+
